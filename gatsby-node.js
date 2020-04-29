c438f18b866e0bf9cec688f119d6acf25b81cba4
Lukasz Rej <rejlukasz@gmail.com>
2020-04-29 14:43:25 +0200

    Babel-polyfill added

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,15 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+exports.modifyWebpackConfig = function(config, env) {
+    if (env === 'build-javascript' || env === 'develop') {
+        const previous = config.resolve().entry
+        config._config.entry = []
+        config.merge({
+            entry: ['babel-polyfill'].concat(previous)
+        })
+    }
+    return config
+}
+
 // You can delete this file if you're not using it
