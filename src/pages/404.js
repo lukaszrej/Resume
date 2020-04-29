f432001bfc87361d6d1c19d0b831adb002c0532e
Lukasz Rej <rejlukasz@gmail.com>
2020-04-29 18:36:31 +0200

    404-page Global style component added to remove delay in loading

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,31 @@
-import React  from "react";
+import React, {useContext} from "react";
 import "../styles/main.scss"
 import { Link } from "gatsby";
 import Head from "../components/Head";
+import Context from "../store/context";
+import {css, Global} from "@emotion/core";
+import { useTheme } from "emotion-theming";
 
 export default () => {
+    const { state } = useContext(Context)
+    const theme = useTheme();
+
     return (
-        <main className="p-container">
-            <Head/>
-            <section className="c-section">
-                <h1 className="c-section__title">Section not found</h1>
-                <p>The page wasn't found. Go back to see the resume.</p>
-                <Link to="/" className="c-button">go back</Link>
-            </section>
-        </main>
+        <>
+            <Global
+                styles={css`                    
+                    body { background-color: ${state.isDark ? theme.dark.background : theme.light.background} }
+                    h1, p, a { color: ${state.isDark ? theme.dark.fontColor : theme.light.fontColor} }
+                `}
+            />
+            <main className="p-container">
+                <Head/>
+                <section className="c-section">
+                    <h1 className="c-section__title">Section not found</h1>
+                    <p>The page wasn't found. Go back to see the resume.</p>
+                    <Link to="/" className="c-button">go back</Link>
+                </section>
+            </main>
+        </>
     )
 };
