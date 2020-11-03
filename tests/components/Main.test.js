2803442596ea68f57144fd22609068e66b6c8756
Lukasz <rejlukasz@gmail.com>
2020-11-03 18:52:03 +0100

    Testing config files added

---
+++ tests/components/Main.test.js
@@ -0,0 +1,10 @@
+// import React from "react";
+// import renderer from "react-test-renderer";
+// import Main from "./Main";
+
+describe("Main component", () => {
+    it("matches the snapshot", () => {
+        // const tree = renderer.create(<Main />).toJSON();
+        // expect(3+2).toBe(5);
+    });
+});
